f9e42a671eaa53dfe12b66e905c338c203675302
jsniezek <jsniezek@gmail.com>
2017-09-25 21:21:09 -0400

    created groups for cards, each with it's own color.

--- canvas.js
+++ canvas.js
@@ -8,16 +8,17 @@ var cards = d3.range(10).map(function(d, i) {
   return {
     x: Math.round(Math.random() * (width - cardWidth * 2) + cardWidth),
     y: Math.round(Math.random() * (height - cardHeight * 2) + cardHeight),
-    key: i
+    key: i,
+    group: null
   };
 });
 
-// var color = d3.scaleOrdinal()
-//     .range(d3.schemeCategory20);
+var numGroups = 0;
 
 var color = d3.scaleSequential().domain([0, 10])
 	.interpolator(d3.interpolateRainbow);
 
+//initial setup of all cards and actions
 svg.selectAll(".card")
   .data(cards)
   .enter().append("g")
@@ -28,6 +29,7 @@ svg.selectAll(".card")
       .on("drag", dragged)
       .on("end", dragended));
 
+//from a blank group, create a new card with a rect and a text element
 function createCardWithText(selection) {
   selection
     .append("rect")
@@ -39,13 +41,15 @@ function createCardWithText(selection) {
       .style("fill", "#fff")
       ;
 
+  //create text element
   selection
-  .append('text')
-    .text(function(d, i) {
-      return "card" + (i + 1);
-    })
-    .attr('x', function(d) { return d.x + 50; })
-    .attr('y', function(d) { return d.y + 50; });
+    .append('text')
+      .text(function(d, i) {
+        return "card" + (i + 1);
+      })
+      .attr('x', function(d) { return d.x + 40; })
+      .attr('y', function(d) { return d.y + 50; })
+      .classed("text", true);
 }
 
 
@@ -54,17 +58,18 @@ function dragstarted(d) {
 }
 
 function dragged(d) {
-  d3.select(this).select("text").attr("x", d.x = d3.event.x + 50).attr("y", d.y = d3.event.y + 50);
+  d3.select(this).select("text").attr("x", d.x = d3.event.x + 40).attr("y", d.y = d3.event.y + 50);
   d3.select(this).select("rect").attr("x", d.x = d3.event.x).attr("y", d.y = d3.event.y);
 }
 
 function dragended(d) {
-  d3.select(this).classed("active", false).call(checkGroup);
+  d3.select(this).classed("active", false).call(checkCollisions);
 }
 
-function checkGroup(selection) {
-  console.log("I am jojo");
-  draggedItem = selection.datum(); //.style("fill", "#343434")
+function checkCollisions(selection) {
+
+  draggedItem = selection.datum();
+
   // find collisions
   var detected = false;
   var items = d3.selectAll("rect").each(function(d) {
@@ -74,19 +79,41 @@ function checkGroup(selection) {
        draggedItem.x + cardWidth > d.x &&
        draggedItem.y < d.y + cardHeight &&
        cardHeight + draggedItem.y > d.y) {
+
         // collision detected.
+        console.log("I am jojo");
         console.log("collision!");
         detected = true;
-        var hitKey = d.key;
-        svg.selectAll("rect").filter(function(d) { return d.key === hitKey}).style("fill", color(d.key));
-        svg.selectAll("rect").filter(function(d) { return d.key === draggedItem.key}).style("fill", color(d.key));
+
+        addToGroup(draggedItem, d);
     }
 
   })
   if (!detected) {
-    svg.selectAll("rect").filter(function(d) { return d.key === draggedItem.key}).style("fill", "#ccc");
+    svg.selectAll("rect").filter(function(d) { return d.key === draggedItem.key}).style("fill", "#ffffff");
   }
   //items.style("fill", "blue");
   // find classes of collided items.
   // select all items with class.
+}
+
+function addToGroup(draggedItem, target) {
+
+  //first, see if target is in a group.
+  if(!target.group)
+  {
+    target.group = numGroups++;
+    console.log(numGroups);
+    svg.selectAll("rect")
+      .filter(function(d)
+      { return d.key === target.key })
+      .style("fill", color(target.group));
+  }
+
+  //now, add dragged item into target item's group
+  draggedItem.group = target.group;
+  svg.selectAll("rect")
+    .filter(function(d)
+    { return d.key === draggedItem.key })
+    .style("fill", color(draggedItem.group));
 }
